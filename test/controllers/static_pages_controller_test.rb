3c70ebdd90d477e56d42fec1c186be93be65d30d
ikurasusshi <kaikai180208@gmail.com>
2025-06-21 15:35:28 +0900

    completed title costomize

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | takaraya reserve app"
+    assert_select "title", "takaraya reserve app"
   end
 end
